b6d34976f06d52bfca1fbb82048520fe4be5d0bb
Chongyang Zhou <chongyang.zhou970221@gmail.com>
2025-03-30 21:17:31 +1100

    4.2c first commit

--- index.js
+++ index.js
@@ -95,6 +95,47 @@ app.get("/divide", (req, res) => {
     }
 });
 
+//Power
+
+const power = (n1, n2) => {
+    return Math.pow(n1, n2);//built in function to calculate power
+}
+
+app.get("/power", (req, res) => {
+    try {
+        const n1 = parseFloat(req.query.n1);
+        const n2 = parseFloat(req.query.n2);
+        if (isNaN(n1)) throw new Error("n1 incorrectly defined");
+        if (isNaN(n2)) throw new Error("n2 incorrectly defined");
+        const result = power(n1, n2);
+        res.status(200).json({ statusCode: 200, data: result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ statusCode: 500, msg: error.toString() });
+    }
+});
+
+
+// Square root
+
+const sqrt = (n1) => {
+    if (n1 < 0) throw new Error("Cannot take square root of a negative number"); // error handling specially for negative numbers
+    return Math.sqrt(n1);// built in function to calculate square root
+}
+
+app.get("/sqrt", (req, res) => {
+    try {
+        const n1 = parseFloat(req.query.n1);
+        if (isNaN(n1)) throw new Error("n1 incorrectly defined");
+        const result = sqrt(n1);
+        res.status(200).json({ statusCode: 200, data: result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ statusCode: 500, msg: error.toString() });
+    }
+});
+
+
 const port = 3040;
 app.listen(port, () => {
     console.log("Listening on port " + port);
